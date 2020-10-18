55de0108c40d9c9201432a8bf528ead54280a63c
guistetter <guilherme_stetter@hotmail.com>
2020-10-18 12:13:44 -0300

    iniciando app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './config/ReactotronConfig';
+import Routes from './routes';
+
+console.tron.log('hello motha fock');
+
+export default function App() {
+  return <Routes />;
+}
