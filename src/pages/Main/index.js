d366cf73a584958d9fcf099528f4251b3260d636
guistetter <guilherme_stetter@hotmail.com>
2020-10-18 14:43:33 -0300

    add user

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Keyboard} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api'
 
 export default class Main extends Component {
   state = {
@@ -8,8 +10,24 @@ export default class Main extends Component {
     users: [],
   }
 
-  handleAddUser = () => {
+  handleAddUser = async () => {
     console.tron.log(this.state.newUser)
+    const {users, newUser} = this.state
+
+    const response = await api.get(`/users/${newUser}`)
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    }
+
+    this.setState({
+      users:[...users, data],
+      newUser:'',
+    })
+    Keyboard.dismiss()
   }
 
   render(){
