cf24b2323cf0c66bc255862c7a188608af917920
guistetter <guilherme_stetter@hotmail.com>
2020-10-18 14:25:09 -0300

    criando estado para input

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input autoCorret={false}
-        autoCapitalizar="none"
-        placeholder="Adicionar Usuario"
-        />
-        <SubmitButton>
-        <Icon name="add" size={20} color="#FFF"/>
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  }
+
+  handleAddUser = () => {
+    console.tron.log(this.state.newUser)
+  }
+
+  render(){
+    const {newUser} = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input autoCorret={false}
+          autoCapitalizar="none"
+          placeholder="Adicionar Usuario"
+          value={newUser}
+          onChangeText={text => this.setState({newUser: text})}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+          <Icon name="add" size={40} color="#FFF"/>
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
 }
 
 Main.navigationOptions = {
