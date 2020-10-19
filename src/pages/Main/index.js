1138927f3a9848512919fbd493d15f4896bbad08
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 10:05:18 -0300

    create List details of user

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Keyboard} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List, User,Avatar,Name,Bio, ProfileButton, ProfileButtonText } from './styles';
 import api from '../../services/api'
 
 export default class Main extends Component {
@@ -31,7 +31,7 @@ export default class Main extends Component {
   }
 
   render(){
-    const {newUser} = this.state;
+    const {users, newUser} = this.state;
     return (
       <Container>
         <Form>
@@ -48,6 +48,21 @@ export default class Main extends Component {
           <Icon name="add" size={40} color="#FFF"/>
           </SubmitButton>
         </Form>
+
+        <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({item}) => (
+          <User>
+            <Avatar source={{uri: item.avatar}}/>
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+            <ProfileButton onPress={()=>{}}>
+              <ProfileButtonText>Ver Perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+        />
       </Container>
     )
   }
