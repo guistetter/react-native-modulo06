0b4dc8dd0ebd6b9db05aa3780040dc7f6b0e4ab3
guistetter <guilherme_stetter@hotmail.com>
2020-10-19 10:23:14 -0300

    create async storage methods

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,12 +3,27 @@ import {Keyboard, ActivityIndicator} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton, List, User,Avatar,Name,Bio, ProfileButton, ProfileButtonText } from './styles';
 import api from '../../services/api'
+import AsyncStorage from '@react-native-community/async-storage'
 
 export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
     loading: false
+  }
+
+  async componentDidMount(){
+    const users = await AsyncStorage.getItem('users');
+    if(users){
+      this.setState({users: JSON.parse(users)})
+    }
+  }
+
+  async componentDidUpdate(_,prevState){
+    const { users } = this.state
+    if(prevState.users !== users){
+      await AsyncStorage.setItem('users', JSON.stringify(users))
+    }
   }
 
   handleAddUser = async () => {
